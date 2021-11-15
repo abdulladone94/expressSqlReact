a286f9021171aa8e522e451caf4d3530753ebe9a
abdulladone94 <abdulladone94@gmail.com>
2021-11-15 21:32:17 +0530

    Initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <lable>Name:</lable>
+      <input type="text" />
+      <lable>Age:</lable>
+      <input type="number" />
+      <lable>Country:</lable>
+      <input type="text" />
+      <lable>Position:</lable>
+      <input type="text" />
+      <lable>Wages(year):</lable>
+      <input type="number" />
     </div>
   );
 }
