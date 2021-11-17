933d2452e10056d337d89fe9814e156d18cabc72
abdulladone94 <abdulladone94@gmail.com>
2021-11-17 08:44:47 +0530

    setup react useState hook

--- src/App.js
+++ src/App.js
@@ -1,18 +1,53 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("0");
+  const [country, setCountry] = useState("");
+  const [position, setPosition] = useState("");
+  const [wage, setWage] = useState("0");
+
   return (
     <div className="App">
-      <lable>Name:</lable>
-      <input type="text" />
-      <lable>Age:</lable>
-      <input type="number" />
-      <lable>Country:</lable>
-      <input type="text" />
-      <lable>Position:</lable>
-      <input type="text" />
-      <lable>Wages(year):</lable>
-      <input type="number" />
+      <div className="information">
+        <label>Name:</label>
+        <input
+          type="text"
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        />
+        <label>Age:</label>
+        <input
+          type="number"
+          onChange={(e) => {
+            setAge(e.target.value);
+          }}
+        />
+        <label>Country:</label>
+        <input
+          type="text"
+          onChange={(e) => {
+            setCountry(e.target.value);
+          }}
+        />
+        <label>Position:</label>
+        <input
+          type="text"
+          onChange={(e) => {
+            setPosition(e.target.value);
+          }}
+        />
+        <label>Wages(year):</label>
+        <input
+          type="number"
+          onChange={(e) => {
+            setWage(e.target.value);
+          }}
+        />
+        <button>Add Employee</button>
+      </div>
     </div>
   );
 }
